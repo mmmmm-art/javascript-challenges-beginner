cd7d1c6b56c5aa3836a4f9828ca2b68ecb81c742
Thad <cosleytg@s.dcsdk12.org>
2022-09-12 19:04:09 -0600

    Solved 6&7

--- scripts/challenge-6.js
+++ scripts/challenge-6.js
@@ -10,7 +10,25 @@
 // you can log out to the console or to the output container like this:
 // output.innerText = "My output";
 const output = document.getElementById("output-container");
+const output2 = document.getElementById("output-container2");
+const input = document.getElementById("input");
+const button = document.getElementById("Push");
+const calc = document.getElementById("Calculate");
 if (!output) throw "Output container not found.";
 
 //@ts-check
 // your code starts after this line
+
+
+let nums = [];
+button.addEventListener("click", (b) => {
+	nums.push(Number(input.value));
+	output.innerText = nums;
+});
+
+calc.addEventListener("click", (b) => {
+	nums.sort(function (a, b) {  return a - b;  });
+	output2.innerText = nums;
+});
+
+//solved
--- scripts/challenge-7.js
+++ scripts/challenge-7.js
@@ -10,7 +10,24 @@
 // you can log out to the console or to the output container like this:
 // output.innerText = "My output";
 const output = document.getElementById("output-container");
+const output2 = document.getElementById("output-container2");
+const input = document.getElementById("input");
+const button = document.getElementById("Push");
+const calc = document.getElementById("Calculate");
 if (!output) throw "Output container not found.";
 
 //@ts-check
 // your code starts after this line
+
+let nums = [];
+button.addEventListener("click", (b) => {
+	nums.push(Number(input.value));
+	output.innerText = nums;
+});
+
+calc.addEventListener("click", (b) => {
+	nums = nums.filter(n => n > 0);
+	output2.innerText = nums;
+});
+
+//solved
